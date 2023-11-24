e453787d5ca26033f11dc4b85453c7e8baf98d88
Ryan H <ryanzhutch@gmail.com>
2023-11-24 10:52:07 -0700

    Add PWA support to project

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import * as path from 'path';
 import react from '@vitejs/plugin-react-swc';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +11,7 @@ export default defineConfig({
   preview: {
     port: 8080,
   },
-  plugins: [react()],
+  plugins: [react(), VitePWA()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, './src') }],
   },
